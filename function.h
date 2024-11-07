58a36ade9cf55f4d2569a9c92441a74d37808993
unknown <daniillolhigh@mail.ru>
2024-11-07 03:29:14 +0300

    day1

---
+++ function.h
@@ -0,0 +1,42 @@
+#ifndef FUNCTION
+#define FUNCTION
+
+#define eps 1e-15
+#define MAX(n, m) n > m ? n : m
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void print_matrix(double *A, int n); //
+
+void f0(double *A, int n, char *filename, int *err);//
+
+void f1(double *A, int n);//
+
+void f2(double *A, int n);//
+
+void f3(double *A, int n);//
+
+void f4(double *A, int n);//
+
+void fill(double *A, int n, char *filename);//
+
+void fill_right_part(double *A, double *B, int n);//
+
+double norm(double *X, int n);//
+
+double discrepancy_1(double *A, double *B, double *X, int n);//
+
+double discrepancy_2(double *X, int n);//
+
+void print_matrix(double *A, int n);//
+
+void multiply(double *A, double *C, int n);//
+
+void solve_1(double *A, int n, double *B);
+
+void reflection(double *A, int n);
+
+#endif
