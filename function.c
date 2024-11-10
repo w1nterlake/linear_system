7f2d18948316145e4f0c5a81b21dfccb288bb063
unknown <daniillolhigh@mail.ru>
2024-11-10 17:35:22 +0300

    day 4

--- function.c
+++ function.c
@@ -1,35 +1,27 @@
 #include "function.h"
 
-
-void print_matrix(double *A, int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%10.3e ", A[i * n + j]);
-        }
-        printf("\n");
-    }
-}
-
 void f0(double *A, int n, char *filename, int *err){
+    FILE *fin;
     int count = 0;
-    FILE *fin = fopen(filename, "r");
+    fin = fopen(filename, "r");
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(fscanf(fin, "%lf", (A + i*n + j)) == 1){
+            if(fscanf("%lf", A + i * n + j) == 1){
                 count++;
-            }   
+            }
         }
     }
-    if(count == n*n){
+    if(count != n * n){
         *err = 4;
     }
+
     fclose(fin);
 }
 
 void f1(double *A, int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            A[i * n + j] = n + 1 - MAX(i, j);
+            A[i * n + j] = n -MAX(i, j) + 1;
         }
     }
 }
@@ -60,23 +52,24 @@ void f4(double *A, int n){
 
 void fill(double *A, int n, char *filename, int s, int *err){
     switch(s){
-        case 0:
+        case(0):
             f0(A, n, filename, err);
             break;
-        case 1:
+        case(1):
             f1(A, n);
             break;
-        case 2:
+        case(2):
             f2(A, n);
             break;
-        case 3:
+        case(3):
             f3(A, n);
             break;
-        case 4:
+        case(4):
             f4(A, n);
             break;
         default:
             break;
+
     }
 }
 
@@ -89,7 +82,7 @@ void fill_right_part(double *A, double *B, int n){
     }
 }
 
-double norm(double *X, int n){
+double norm_1(double *X, int n){
     double max = 0;
     for(int i = 0; i < n; i++){
         max += fabs(X[i]);
@@ -119,77 +112,24 @@ double discrepancy_2(double *X, int n){
     return sum1/sum2;
 }
 
-// void print_matrix(double *A, int n){
-//     for(int i = 0; i < n; i++){
-//         for(int j = 0; j < n; j++){
-//             printf("%10.3e ", A[i * n + j]);
-//         }
-//         printf("\n");
-//     }
-// }
-
-void multiply(double *A, double *C, int n){
-    double *X = (double*)malloc(n * sizeof(double));
+double scalar(double *A, double *B, int n){
     double sum = 0;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < n; k++){
-                sum += A[i * n + k] * C[k * n + j];
-            }
-            X[j] = sum;
-            sum = 0;
-        }
-        for(int l = 0; l < n; l++){
-            C[l * n + i] = X[l];
-        }
+        sum += A[i] * B[i];
     }
-    free(X);
 }
 
-int scalar(double *X, double *Y, int n){
-    int sum = 0;
-    for(int i = 0; i < n; i++){
-        sum += X[i] * Y[i];
-    }
-    return sum;
+double norm_2(double *X, int n){
+    return sqrt(scalar(X, X, n));
 }
 
-void solve_1(double *A, double *B, int n){
-    double t1, t2;
+void made_vector(double *A, double *X, int n){
+    double tmp = norm_2(A, n);
+    A[0] -= tmp;
+    tmp = norm_2(A, n);
     for(int i = 0; i < n; i++){
-        t1 = 0;
-        for(int j = i; j < n; j++){
-            t1 += A[j * n + i];
-        }
-        t2 = t1;
-        t1 =  sqrt(t1);
-        A[i * n + i] -= t1;
-        t2 = sqrt(t1 + A[i * n + i] * A[i * n + i]);
-        for(int j = i; j < n; j++){
-            A[j * n + i] /= t2;
-        }
-        for(int j = i + 1; j < n; j++){
-            t1 = 0;
-            for(int l = i; l < n; l++){
-                t1 += A[l * n + i] * A[l * n + j];
-            }
-            t1 *= 2;
-            for(int l = i; l < n; l++){
-                A[l * n + j] -= t1 * A[l * n + i];
-            }
-        }
-        t1 = 0;
-        for(int l = i; l < n; l++){
-            t1 += A[l * n + i] * B[l];
-        }
-        t1 *= 2;
-        for(int l = i; l < n; l++){
-            B[l] -= t1 * A[l * n + i];
-        }
-        for(int k = i + 1; k < n; k++){
-            A[k * n + i] = 0;
-        }
-        A[i * n + i] = t2;
+        A[i] /= tmp;
+        X[i] = A[i];
     }
 }
 
